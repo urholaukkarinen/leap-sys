65bceea39edabe995a7ebf7887fc5c2003d82821
Urho Laukkarinen <urho.laukkarinen@gmail.com>
2020-05-01 01:10:43 +0300

    Bindings are only generated with buildtime_bindgen
    Bindings are now generated only if buildtime_bindgen feature is enabled for the build

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
-use bindgen;
+#[cfg(feature = "buildtime_bindgen")]
+extern crate bindgen;
 
 fn main() {
-    let bindings = bindgen::Builder::default()
+    #[cfg(feature = "buildtime_bindgen")]
+    bindgen::Builder::default()
         .header("LeapC.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
-        .expect("Unable to generate bindings");
-
-    bindings
+        .expect("Unable to generate bindings")
         .write_to_file("src/lib.rs")
         .expect("Couldn't write bindings!");
 }
